bc556eddca5ff290f6533fce957a0e51c99f4116
Daryl van Loon <Daryl.VanLoon@narato.be>
2017-12-13 15:14:58 +0100

    updated test

--- JsonDiffXUnitTests/UnitTest1.cs
+++ JsonDiffXUnitTests/UnitTest1.cs
@@ -5,10 +5,10 @@ using Newtonsoft.Json.Linq;
 
 namespace JsonDiffXUnitTests
 {
-    public class UnitTest1
+    public class Difftests
     {
         [Fact]
-        public void UpdateSchemaTest()
+        public void UpdateJsonTest()
         {
             #region jsonDefinitions
             string jsonValidationSchema = @"
@@ -66,7 +66,74 @@ namespace JsonDiffXUnitTests
             var newOne = JToken.Parse(updatedJsonValidationSchema);
 
             var result = new JsonDiffer().Diff(existing, newOne, false);
-            PatchDocument test2 = result;
+            var addOperations = result.Operations.FindAll(x => x.GetType() == typeof(AddOperation));
+            var replaceOperations = result.Operations.FindAll(x => x.GetType() == typeof(ReplaceOperation));
+            Assert.True(addOperations.Count == 4);
+            Assert.True(replaceOperations.Count == 1);
+
         }
+
+        [Fact]
+        public void RemovePropertyTest()
+        {
+            #region jsonDefinitions
+            string jsonValidationSchema = @"
+               {
+                ""$schema"": ""http://json-schema.org/draft-04/schema#"",
+                ""title"": ""Person"",
+                ""type"": ""object"",
+					""properties"": {
+						""firstName"": {
+							""type"": ""string""
+						},
+						""lastName"": {
+							""type"": ""string""
+						},
+						""age"": {
+							""description"": ""Age in years"",
+							""type"": ""integer""
+						}					                 
+                    },               
+                ""additionalProperties"": false               
+             }";
+
+            string updatedJsonValidationSchema = @"
+               {
+                ""$schema"": ""http://json-schema.org/draft-04/schema#"",
+                ""title"": ""Person"",
+                ""type"": ""object"",
+					""properties"": {
+						""firstName"": {
+							""type"": ""string"",
+                            ""additionalProperties"": false               
+                            
+						},
+						""lastName"": {
+                            ""additionalProperties"": false               
+                
+						},
+						""age"": {
+							""description"": ""Age in years"",
+							""type"": ""string"",
+                            ""additionalProperties"": false
+						},
+                        ""email"": {
+                            ""description"": ""Private email address"",
+                            ""type"": ""string"",
+                            ""additionalProperties"": false
+                        }					                 
+                    },               
+                ""additionalProperties"": false               
+             }";
+            #endregion
+
+            var existing = JToken.Parse(jsonValidationSchema);
+            var newOne = JToken.Parse(updatedJsonValidationSchema);
+
+            var result = new JsonDiffer().Diff(existing, newOne, false);
+            var removeOperations = result.Operations.FindAll(x => x.GetType() == typeof(RemoveOperation));
+            Assert.True(removeOperations.Count == 1);
+        }
+
     }
 }
